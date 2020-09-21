2a996cd83dd582c09d1b02e6b18742c489051700
chibiherbie <bekkerrdm@gmail.com>
2020-09-21 12:49:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/events/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void OnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
